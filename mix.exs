bc9658ee9f101214311f13f5ed62fc3587f7a64e
Eduardo <eduardo@sendle.com>
2019-12-13 17:01:52 +1300

    Bump version 1.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mimic.Mixfile do
   def project do
     [
       app: :mimic,
-      version: "1.1.2",
+      version: "1.1.3",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
