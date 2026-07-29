e981b4336f2c4746ff879f6e82367718015cdf75
Grant West <grant.west@river.com>
2026-07-28 20:06:21 -0400

    test: demonstration test for verify on exit cleanup bug Run test with `mix test test/edge_case_test.exs --seed 0`

--- test/edge_case_test.exs
+++ test/edge_case_test.exs
@@ -20,5 +20,32 @@ defmodule Mimic.EdgeCase do
         verify!(child_pid)
       end
     end
+
+  end
+
+  ################### Bug demonstration ###################
+  # Run with `mix test test/edge_case_test.exs --seed 0`
+  # Because it depends on the order of the tests
+  describe "failed verification in global mode" do
+    # must be in global mode
+    setup :set_mimic_global
+    # must verify on exit
+    setup :verify_on_exit!
+
+    # this test must run first
+    test "reports an unmet expectation on exit" do
+      # must have expecation remaining
+      expect(Calculator, :add, 2, fn x, y -> x + y end)
+      assert Calculator.add(1, 2) == 3
+    end
+  end
+
+  describe "the test after a failed global verification" do
+    # this test must run after the previous test
+    test "can use Mimic" do
+      # this stub fails because the global owner was never cleaned up
+      stub(Calculator, :add, fn _, _ -> 0 end)
+      assert Calculator.add(1, 2) == 0
+    end
   end
 end
